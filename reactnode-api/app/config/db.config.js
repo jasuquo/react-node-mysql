964be03c5181c9ff671c1fb20e7e7f242f89251a
joemile <joemile@macs-MacBook-Air.local>
2022-07-19 12:27:55 +0100

    config files

---
+++ reactnode-api/app/config/db.config.js
@@ -0,0 +1,28 @@
+// module.exports = {
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "123456",
+  // DB: "testdb",
+  // dialect: "mysql",
+  // pool: {
+    // max: 5,
+    // min: 0,
+    // acquire: 30000,
+    // idle: 10000
+  // }
+// };
+
+module.exports = {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
